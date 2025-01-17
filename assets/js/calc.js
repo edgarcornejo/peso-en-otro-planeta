ff7f29f4b2c90ecf714fe8747184f472fa691006
Edgar Cornejo <spens.4562@gmail.com>
2025-01-17 13:07:21 -0500

    cambios de css

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,14 +1,14 @@
-var boton = document.getElementById("btn_calcular");
-boton.addEventListener("click", fcalcular);
+var select = document.getElementById("planetas");
+select.addEventListener("change", calcular);
 
 /*var nombre_planeta;*/
 var g_tierra = 9.8;
 
-function fcalcular() {
+function calcular() {
   var peso = document.getElementById("txt_peso").value;
   var select = document.getElementById("planetas").value;
   peso_final = peso * select / g_tierra;
-  document.getElementById("resultado").innerHTML = "Pesarias " + parseInt(peso_final) + " kilos";
+  document.getElementById("resultado").innerHTML = "Pesarías: " + parseInt(peso_final) + " kilos";
 }
 
 onload = function () {
